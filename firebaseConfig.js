3d782594fe5619dd95cfc875d4944191c82ca9c4
alimc17 <alimcampbell2005@gmail.com>
2025-04-06 21:24:00 -0400

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSTYnrHfoA-OQWvvjgd0Kbhu8AN9tZuGs",
+  authDomain: "pokemon-clicker-game.firebaseapp.com",
+  projectId: "pokemon-clicker-game",
+  storageBucket: "pokemon-clicker-game.firebasestorage.app",
+  messagingSenderId: "1030266760892",
+  appId: "1:1030266760892:web:2928b7bb06063353a9eab9",
+  measurementId: "G-EXH6J632CY"
+};
+
+export default firebaseConfig
