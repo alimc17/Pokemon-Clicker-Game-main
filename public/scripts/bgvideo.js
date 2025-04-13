2fbbfd28c1d77809d2197d8e56463bace58194a2
Peter Gyan <petergyan001@gmail.com>
2025-04-13 19:33:36 -0400

    bg video
    added script to hopefully fix it

---
+++ public/scripts/bgvideo.js
@@ -0,0 +1,9 @@
+// genuienly why does it not work on firebase, this better fix it
+window.addEventListener("load", function () {
+    const bgVideo = document.querySelector(".bg-video");
+    if (bgVideo) {
+      bgVideo.play().catch((err) => {
+        console.warn("Autoplay prevented:", err);
+      });
+    }
+  });
