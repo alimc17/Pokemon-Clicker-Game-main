66c5c3df46b5c8d1f264d69f6b9333bdf1ba7b52
Peter Gyan <petergyan001@gmail.com>
2025-04-13 21:56:54 -0400

    the exact same as before
    the changes poofed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -26,3 +26,22 @@ window.addEventListener('beforeunload', () => {
         updateGameProgress({ gameData: { pTotal: window.pTotal } });
     }
 });
+
+function buyUpg() {
+    const upgCost = document.querySelector('.upg-cost');
+    const upgLevel = document.querySelector('.upg-level')
+    const cost = parseInt(upgCost.textContent);
+
+    if (window.pTotal >= cost) {
+        window.pTotal -= cost;
+        document.querySelector('.p-total').textContent = window.pTotal;
+        upgLevel.textContent = parseInt(upgLevel.textContent) + 1;
+
+        const user = firebase.auth().currentUser;
+        if (user) {
+            updateGameProgress({ gameData: { pTotal: window.pTotal } });
+        } else {
+            localStorage.setItem('guestProgress', window.pTotal.toString());
+        }
+    }
+}
