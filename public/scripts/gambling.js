ac6b50deced44dba7e98c1be695905897b02aea6
Peter Gyan <petergyan001@gmail.com>
2025-04-17 00:03:36 -0400

    bling
    literally all of gambling but for some reason i cannot get tooltips to work (they worked before and just stopped it's been an hour help)

--- public/scripts/gambling.js
+++ public/scripts/gambling.js
@@ -37,43 +37,117 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const legendaries = [
     {
+        name: 'Articuno',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/144.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/144.ogg',
+    },
+    {
+        name: 'Zapados',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/145.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/145.ogg',
+    },
+    {
+        name: 'Moltres',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/146.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/146.ogg',
+    },
+    {
         name: 'Mewtwo',
         img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/150.ogg',
     },
     {
         name: 'Lugia',
         img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/249.ogg',
     },
     {
         name: 'Ho-Oh',
         img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/250.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/250.ogg',
     },
     {
-        name: 'Latias',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/380.png',
+        name: 'Kyogre',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/382.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/382.ogg',
     },
     {
-        name: 'Latios',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/381.png',
+        name: 'Groudon',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/383.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/383.ogg',
     },
     {
-        name: 'Lugia',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png',
+        name: 'Rayquaza',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/384.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/384.ogg',
     },
     {
-        name: 'Mewtwo',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png',
+        name: 'Dialga',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/483.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/483.ogg',
     },
     {
-        name: 'Lugia',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png',
+        name: 'Palkia',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/484.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/484.ogg',
     },
     {
-        name: 'Mewtwo',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png',
+        name: 'Regigigas',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/486.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/486.ogg',
     },
     {
-        name: 'Lugia',
-        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png',
+        name: 'Girantina',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/487.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/487.ogg',
+    },
+    {
+        name: 'Reshiram',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/643.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/643.ogg',
+    },
+    {
+        name: 'Zekrom',
+        img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/644.png',
+        cry: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/644.ogg',
     },
 ]
+
+const unlockedLegendaries = new Array(15).fill(false);
+
+const legendaryButton = document.querySelector('.buy-button');
+legendaryButton.addEventListener('click', buyLegendary);
+
+function buyLegendary() {
+    if (parsedPTotal < 5) return;
+
+    const lockedIndexes = unlockedLegendaries
+        .map((unlocked, index) => !unlocked ? index : null)
+        .filter(index => index !== null);
+
+    if (lockedIndexes.length === 0) return;
+
+    const randomIndex = lockedIndexes[Math.floor(Math.random() * lockedIndexes.length)];
+    const legendary = legendaries[randomIndex];
+
+    const slot = document.querySelector(`.question-slot[data-index="${randomIndex}"]`);
+    slot.innerHTML = `<img src="${legendary.img}" alt="${legendary.name}" style="width: 100%; height: auto;" draggable="false"/>`;
+
+    // Add tooltip and animation class
+    slot.setAttribute('data-name', legendary.name);
+    slot.classList.add('legendary-reveal');
+    if (!slot.classList.contains('legendary-slot')) {
+        slot.classList.add('legendary-slot');
+    }
+
+    // Mark as unlocked
+    unlockedLegendaries[randomIndex] = true;
+
+    const cryAudio = new Audio(legendary.cry);
+    cryAudio.volume = 0.4;
+    cryAudio.play();
+
+    // Deduct pokédollars
+    parsedPTotal -= 5;
+    pTotal.innerHTML = Math.round(parsedPTotal);
+}
